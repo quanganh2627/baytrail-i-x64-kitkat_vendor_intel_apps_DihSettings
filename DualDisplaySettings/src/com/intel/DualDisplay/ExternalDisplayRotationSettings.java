b23ef6d04437e30c80706e79c472503c23fe63ef
jianping.li <jianpingx.li@intel.com>
2015-02-25 14:48:34 +0800

    Add one new rotation status "none" on main display
    This new status is for CTS test.
    
    Change-Id: I619cbe9dcc8b0a2c2de3433508bc605c1fdb48d8
    Signed-off-by: jianping.li <jianpingx.li@intel.com>
    Signed-off-by: Chon Ming Lee <chon.ming.lee@intel.com>

--- DualDisplaySettings/src/com/intel/DualDisplay/ExternalDisplayRotationSettings.java
+++ DualDisplaySettings/src/com/intel/DualDisplay/ExternalDisplayRotationSettings.java
@@ -9,22 +9,25 @@ import android.view.Window;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.FrameLayout;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
+
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
 
 import com.android.internal.app.AlertActivity;
 
 
 public class ExternalDisplayRotationSettings extends AlertActivity implements OnCheckedChangeListener {
-	
+    public String TAG = "ExternalDisplayRotationSettings";
     protected TextView mTitleView;
     protected FrameLayout mContentFrame;
     public RadioButton mPortrait;
     public RadioButton mLandscape;
 
-    public RadioButton mMainPortrait;
-    public RadioButton mMainLandscape;
+    public CheckBox mMainPortrait;
+    public CheckBox mMainLandscape;
 
     public String mSelectedRotation;
     public String mOldSelectedRotation;
@@ -34,6 +37,7 @@ public class ExternalDisplayRotationSettings extends AlertActivity implements On
 
     public String ROTATION_PORTRAIT = "portrait";
     public String ROTATION_LANDSCAPE = "landscape";
+    public String ROTATION_NONE = "none";
     public boolean mSettingChange = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,34 +51,37 @@ public class ExternalDisplayRotationSettings extends AlertActivity implements On
         mPortrait = (RadioButton) findViewById(R.id.radioButtonPortrait);
         mLandscape = (RadioButton) findViewById(R.id.radioButtonLandscape);
 
-        mMainPortrait = (RadioButton) findViewById(R.id.radioButtonMainPortrait);
-        mMainLandscape = (RadioButton) findViewById(R.id.radioButtonMainLandscape);
+        mMainPortrait = (CheckBox) findViewById(R.id.checkboxMainPortrait);
+        mMainLandscape = (CheckBox) findViewById(R.id.checkboxMainLandscape);
 
         mPortrait.setOnCheckedChangeListener(this);
         mLandscape.setOnCheckedChangeListener(this);
 
-	 mMainPortrait.setOnCheckedChangeListener(this);
+        mMainPortrait.setOnCheckedChangeListener(this);
         mMainLandscape.setOnCheckedChangeListener(this);
 
         mOldSelectedRotation = Settings.System.getStringForUser(getContentResolver(),
                 Settings.System.DISPLAY_ROTATION_ON_EXTERNAL,UserHandle.USER_CURRENT);
-	    if(mOldSelectedRotation.equals(ROTATION_PORTRAIT)){
-                mPortrait.setChecked(true);
-                mLandscape.setChecked(false);
-	    }else{
-            mPortrait.setChecked(false);
-            mLandscape.setChecked(true);
-	    }
+        if(mOldSelectedRotation.equals(ROTATION_PORTRAIT)){
+              mPortrait.setChecked(true);
+              mLandscape.setChecked(false);
+        }else if(mOldSelectedRotation.equals(ROTATION_LANDSCAPE)){
+              mPortrait.setChecked(false);
+              mLandscape.setChecked(true);
+        }
 
         mOldMainSelectedRotation = Settings.System.getStringForUser(getContentResolver(),
                 Settings.System.DISPLAY_ROTATION_ON_MAIN,UserHandle.USER_CURRENT);
-	    if(mOldMainSelectedRotation.equals(ROTATION_PORTRAIT)){
-                mMainPortrait.setChecked(true);
-                mMainLandscape.setChecked(false);
-	    }else{
-            mMainPortrait.setChecked(false);
-            mMainLandscape.setChecked(true);
-	    }
+        if(mOldMainSelectedRotation.equals(ROTATION_PORTRAIT)){
+              mMainPortrait.setChecked(true);
+              mMainLandscape.setChecked(false);
+        }else if(mOldMainSelectedRotation.equals(ROTATION_LANDSCAPE)){
+              mMainPortrait.setChecked(false);
+              mMainLandscape.setChecked(true);
+        }else{
+              mMainPortrait.setChecked(false);
+              mMainLandscape.setChecked(false);
+         }
     }
 
     public void setHeading(int titleRes) {
@@ -94,19 +101,20 @@ public class ExternalDisplayRotationSettings extends AlertActivity implements On
     protected void onStop() {
         super.onStop();
         if(mSelectedRotation==null||mSelectedRotation.isEmpty()
-			|| mMainSelectedRotation ==null||mMainSelectedRotation.isEmpty())
-        	return;
+             || mMainSelectedRotation ==null||mMainSelectedRotation.isEmpty())
+            return;
         mSettingChange = !mOldSelectedRotation.equals(mSelectedRotation);
         if(mSettingChange){
             Settings.System.putString(getContentResolver(),
                 Settings.System.DISPLAY_ROTATION_ON_EXTERNAL,mSelectedRotation);
-//	         Toast.makeText(this,"You must reboot devices to apply the change",Toast.LENGTH_SHORT).show();
+//         Toast.makeText(this,"You must reboot devices to apply the change",Toast.LENGTH_SHORT).show();
         }
-	 mSettingChange = !mOldMainSelectedRotation.equals(mMainSelectedRotation);
+        mSettingChange = !mOldMainSelectedRotation.equals(mMainSelectedRotation);
         if(mSettingChange){
+            Log.d(TAG,"Save mMainSelectedRotation to database :" + mMainSelectedRotation);
             Settings.System.putString(getContentResolver(),
                 Settings.System.DISPLAY_ROTATION_ON_MAIN,mMainSelectedRotation);
-//	         Toast.makeText(this,"You must reboot devices to apply the change",Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this,"You must reboot devices to apply the change",Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -118,35 +126,39 @@ public class ExternalDisplayRotationSettings extends AlertActivity implements On
         }
     }
 
-	@Override
-	public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
-		// TODO Auto-generated method stub
-		if(arg0 == mPortrait){
-			if(arg1){
-				mLandscape.setChecked(false);
-				mSelectedRotation = ROTATION_PORTRAIT;
-			}
-		}
-		if(arg0 ==mLandscape ){
-			if(arg1){
-				mPortrait.setChecked(false);
-				mSelectedRotation = ROTATION_LANDSCAPE;
-			}
-		}
-
-		if(arg0 == mMainPortrait){
-		    if(arg1){
-                     mMainLandscape.setChecked(false);
-                     mMainSelectedRotation = ROTATION_PORTRAIT;
-                  }
+     @Override
+     public void onCheckedChanged(CompoundButton radioButton, boolean isChecked) {
+       // TODO Auto-generated method stub
+        if(radioButton == mPortrait){
+           if(isChecked){
+              mLandscape.setChecked(false);
+              mSelectedRotation = ROTATION_PORTRAIT;
+            }
+         }
+        if(radioButton ==mLandscape ){
+           if(isChecked){
+               mPortrait.setChecked(false);
+               mSelectedRotation = ROTATION_LANDSCAPE;
              }
-             if(arg0 ==mMainLandscape ){
-                  if(arg1){
-                      mMainPortrait.setChecked(false);
-                      mMainSelectedRotation = ROTATION_LANDSCAPE;
-                  }
-              }
-
          }
 
+        if(radioButton == mMainPortrait){
+           if(isChecked){
+              mMainLandscape.setChecked(false);
+              mMainSelectedRotation = ROTATION_PORTRAIT;
+           }else{
+              mMainPortrait.setChecked(false);
+              mMainSelectedRotation = ROTATION_NONE;
+           }
+         }
+         if(radioButton ==mMainLandscape ){
+            if(isChecked){
+               mMainPortrait.setChecked(false);
+               mMainSelectedRotation = ROTATION_LANDSCAPE;
+             }else{
+               mMainLandscape.setChecked(false);
+               mMainSelectedRotation = ROTATION_NONE;
+             }
+          }
+      }
 }
